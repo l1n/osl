3bdc86e6422ad5f65dc88d176678a22ede2619d2
l1n <lin@anticlack.com>
2016-02-07 19:38:11 -0500

    Updated sorting algorithm

--- orgListGen.sh
+++ orgListGen.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl http://osl.umbc.edu/orgs/list/ | grep orgs/detail -A 4 | perl -ne'BEGIN {$/ = "--\n"}' -e'm{detail/(\d*)"}; print $1, "\t";' -e'm{-name">(.*)</span>$}m; print $1, "\t";' -e'm{-cat">(.*)</span>$}m; print $1, "\n";' > orgList.tsv && git add orgList.tsv && git commit -m "Update $(date)"
+curl http://osl.umbc.edu/orgs/list/ | grep orgs/detail -A 4 | perl -ne'BEGIN {$/ = "--\n"}' -e'm{detail/(\d*)"}; print $1, "\t";' -e'm{-name">(.*)</span>$}m; print $1, "\t";' -e'm{-cat">(.*)</span>$}m; print $1, "\n";' | sort -s -n -k 1,1 > orgList.tsv && git add orgList.tsv && git commit -m "Update $(date)" && git push
